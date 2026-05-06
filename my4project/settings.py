4c42f1490e4d541caaa849801017b26d627da118
Aditya <aditya123@gmail.com>
2026-05-06 18:19:39 +0530

    fix allowed hosts properly

--- my4project/settings.py
+++ my4project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1r1+ra5!ftzmxe(g4f7*10x64v!-lbt@isb$nsynmsu63cqlqc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['django2-project-1.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
